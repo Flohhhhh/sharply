b9b3633cc6a3841260501750bdcac7a80a4db34e
Floh <48927090+Flohhhhh@users.noreply.github.com>
2025-10-02 15:32:58 -0400

    Update route.ts

--- src/app/api/genres/route.ts
+++ src/app/api/genres/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import { GENRES } from "~/lib/constants";
 
 export async function GET() {
-  return NextResponse.json(GENRES);
+  return NextResponse.json({ genres: GENRES });
 }
