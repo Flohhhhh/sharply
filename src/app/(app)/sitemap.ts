138a7cbe43331d964923e82fa2af50631f6f59ad
Floh <48927090+Flohhhhh@users.noreply.github.com>
2025-10-18 12:50:06 -0400

    Update sitemap.ts

--- src/app/(app)/sitemap.ts
+++ src/app/(app)/sitemap.ts
@@ -10,7 +10,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: "https://sharplyphoto.com",
       lastModified: new Date(),
-      changeFrequency: "hourly" as const,
+      changeFrequency: "daily" as const,
       priority: 1,
     },
     {
