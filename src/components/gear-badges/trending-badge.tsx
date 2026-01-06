318b2e48f0fc1b61822d1bc65ee89af89c286e77
Floh <48927090+Flohhhhh@users.noreply.github.com>
2026-01-06 14:45:21 -0500

    Update trending-badge.tsx

--- src/components/gear-badges/trending-badge.tsx
+++ src/components/gear-badges/trending-badge.tsx
@@ -3,7 +3,10 @@ import { Badge } from "../ui/badge";
 
 export function TrendingBadge() {
   return (
-    <Badge variant="secondary" className="gap-1 bg-orange-500">
+    <Badge
+      variant="secondary"
+      className="gap-1 bg-orange-400 dark:bg-orange-600"
+    >
       <FlameIcon />
       Trending
     </Badge>
