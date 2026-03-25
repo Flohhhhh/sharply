1f30abe116637c848fd27d6d4d32cca151b6465d
Floh <48927090+Flohhhhh@users.noreply.github.com>
2026-03-25 13:28:21 -0400

    Fix formatting of text for Nikon 50mm lens entry

--- src/app/(app)/(pages)/lists/hall-of-fame/data.ts
+++ src/app/(app)/(pages)/lists/hall-of-fame/data.ts
@@ -43,6 +43,6 @@ export const hallOfFameItems: HallOfFameItem[] = [
   },
   {
     slug: "nikon-af-s-nikkor-50mm-f-1-8g",
-    text: "In an ultra-competitive slice of the lens market, 50mm prime lenses, Nikon's AF-S 50mm 1.8G could be one of the most well-known. It is an excellent and budget friendly full frame 50mm prime lens option for F-Mount. As possibly one of Nikon's best selling lenses ever, it has such good value that there's almost no reason to even look at any of the pricier options unless you're a professional looking for the most performance. Many years later it's still very often recommended to new photographers, and used by professionals.
+    text: "In an ultra-competitive slice of the lens market, 50mm prime lenses, Nikon's AF-S 50mm 1.8G could be one of the most well-known. It is an excellent and budget friendly full frame 50mm prime lens option for F-Mount. As possibly one of Nikon's best selling lenses ever, it has such good value that there's almost no reason to even look at any of the pricier options unless you're a professional looking for the most performance. Many years later it's still very often recommended to new photographers, and used by professionals.",
   },
 ];
